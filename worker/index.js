dc2b855b580fbb1a368629b2186cc8c5ee8b6718
harsh-c <harsh_chauhan94@hotmail.com>
2020-02-12 13:54:44 -0800

    Adding Cron job and setting up Express

--- worker/index.js
+++ worker/index.js
@@ -1,5 +1,10 @@
 var CronJob = require('cron').CronJob;
-var job = new CronJob('* * * * * *', function() {
-  console.log('You will see this message every second');
-}, null, true, 'America/Los_Angeles');
-// job.start();
+
+const fetchGithub = require('./tasks/fetch-github.js')
+
+//fetch github jobs
+var job = new CronJob('* * * * *', fetchGithub, null, true, 'America/Los_Angeles');
+
+
+// we can fetch indeed, stackoverflow here
+// create cron jobs for every fetch 
